431941f3f66d82b870910a6783764ab184739751
Janice <janicelaset@gmail.com>
2015-10-23 20:36:49 -0700

    allow users to edit daycare

--- app/controllers/daycares_controller.rb
+++ app/controllers/daycares_controller.rb
@@ -3,10 +3,6 @@ class DaycaresController < ApplicationController
     @daycare = Daycare.new
   end
 
-  def show
-    @daycare = Daycare.find(params[:id])
-  end
-
   def create
     @daycare = Daycare.new(daycare_params)
     @user = User.find(params[:user_id])
@@ -18,6 +14,26 @@ class DaycaresController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @daycare = Daycare.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @daycare = Daycare.find(params[:id])
+
+    if @daycare.update(daycare_params)
+      redirect_to user_daycare_path(@user, @daycare)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @daycare = Daycare.find(params[:id])
+  end
+
 
   private
   def daycare_params
--- spec/features/daycare_crud_spec.rb
+++ spec/features/daycare_crud_spec.rb
@@ -22,4 +22,26 @@ describe "the daycare crud process" do
     click_button 'Create Daycare'
     expect(page).to have_content 'Please fix these errors'
   end
+
+  it "allows user to update daycare" do
+    daycare = FactoryGirl.create(:daycare)
+    user = daycare.user
+    login_as(user, :scope => :user)
+    visit edit_user_daycare_path(user, daycare)
+    fill_in 'Name', :with => 'Daycare'
+    fill_in 'daycare_structure', :with => 'home'
+    click_button 'Update Daycare'
+    expect(page).to have_content 'Daycare'
+  end
+
+  it "displays errors with daycare update method" do
+    daycare = FactoryGirl.create(:daycare)
+    user = daycare.user
+    login_as(user, :scope => :user)
+    visit edit_user_daycare_path(user, daycare)
+    fill_in 'Name', :with => ''
+    fill_in 'daycare_structure', :with => 'home'
+    click_button 'Update Daycare'
+    expect(page).to have_content 'Please fix these errors'
+  end
 end
