408a37921764cc7cc14a58401b120effa2390195
Janice <janicelaset@gmail.com>
2015-10-27 07:06:16 -0700

    add and test image-daycare association

--- app/models/daycare.rb
+++ app/models/daycare.rb
@@ -1,5 +1,6 @@
 class Daycare < ActiveRecord::Base
   belongs_to :user
+  has_many :images
 
   validates :name, :presence => true
 end
--- spec/models/daycare_spec.rb
+++ spec/models/daycare_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Daycare, type: :model do
-  it {should belong_to :user}
+  it { should belong_to :user }
+  it { should have_many :images }
   it { should validate_presence_of :name }
 end
