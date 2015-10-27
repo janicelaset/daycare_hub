37274891c5e5609d527ea0a664f24609e53b87bc
Janice <janicelaset@gmail.com>
2015-10-27 14:08:00 -0700

    adding paperclip - unable to install imagemagick

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,6 +3,7 @@ class ImagesController < ApplicationController
     @images = current_user.daycare.images
   end
 
+
   def new
     @daycare = current_user.daycare
     @image = Image.new
@@ -11,5 +12,22 @@ class ImagesController < ApplicationController
       format.html
       format.js
     end
+  end
+
+  def create
+    @daycare = Daycare.find(params[:daycare_id])
+    @image = Image.new(image_params)
+    @image.daycare = @daycare
+
+    if @image.save
+      redirect_to edit_user_path(current_user)
+    else
+      flash[:notice] = "Image not saved"
+    end
+  end
+
+  private
+  def image_params
+    params.require(:image).permit(:picture, :description)
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ActiveRecord::Base
   belongs_to :daycare
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
