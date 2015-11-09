d27db05689281bbc1df1b3a82b41c2ee41654052
Janice <janicelaset@gmail.com>
2015-11-09 12:03:42 -0800

    format carousel photo size

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   belongs_to :daycare
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "150x150>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, styles: { large: "940x705", thumb: "150x150>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 
   def image_url
