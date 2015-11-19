9373225c418fa4d9cd6c28a4b7e46edfc166e1fc
Janice <janicelaset@gmail.com>
2015-11-18 16:11:49 -0800

    test image delete for daycare

--- spec/features/image_spec.rb
+++ spec/features/image_spec.rb
@@ -14,4 +14,15 @@ describe "Image crud process" do
     # find('#menu-view-daycare').click
     expect(page).to have_content 'daycare.jpg'
   end
+
+  it "allows user to delete image for daycare", js: true do
+    daycare = FactoryGirl.create(:daycare)
+    user = daycare.user
+    login_as(user, :scope => :user)
+    visit edit_user_daycare_path(user, daycare)
+    click_link 'daycare-add-image'
+    fill_in 'image-edit-desc', :with => 'Photo Description'
+    attach_file('image[picture]', 'spec/images/daycare.jpg')
+    expect(page).to_not have_content 'daycare.jpg'
+  end
 end
