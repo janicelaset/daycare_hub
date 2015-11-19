a4beee1b5009341a5276b0beb9454297e75bd0a8
Janice <janicelaset@gmail.com>
2015-11-18 16:44:44 -0800

    testing image delete (need to wait for attach file complete)

--- spec/features/image_spec.rb
+++ spec/features/image_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'spec_helper'
 
 describe "Image crud process" do
   it "allows user to add image to daycare", js: true do
@@ -23,6 +24,18 @@ describe "Image crud process" do
     click_link 'daycare-add-image'
     fill_in 'image-edit-desc', :with => 'Photo Description'
     attach_file('image[picture]', 'spec/images/daycare.jpg')
+    find("#image-delete-#{daycare.images.first.id}").trigger('click')  #icon is on draggable panel header(overlapping elements)
     expect(page).to_not have_content 'daycare.jpg'
   end
+
+  # it "allows user to update image for daycare", js: true do
+  #   daycare = FactoryGirl.create(:daycare)
+  #   user = daycare.user
+  #   login_as(user, :scope => :user)
+  #   visit edit_user_daycare_path(user, daycare)
+  #   click_link 'daycare-add-image'
+  #   fill_in 'image-edit-desc', :with => 'Photo Description'
+  #   attach_file('image[picture]', 'spec/images/daycare.jpg')
+  #   expect(page).to_not have_content 'daycare.jpg'
+  # end
 end
