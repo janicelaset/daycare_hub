6673234320972f5699cabeb20dd901de97a6e2b7
Janice Laset-Parkerson <janicelaset@Janice-Laset-Parkersons-MacBook.local>
2015-11-30 20:43:56 -0800

    add s3 configuration

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'jquery-ui-rails'
 gem 'acts_as_list'
 gem 'jquery-turbolinks'
 gem 'remotipart', '~> 1.2'
+gem 'aws-sdk'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,13 @@ Rails.application.configure do
 
   # config/environments/production.rb
 # config.action_controller.asset_host = "d2sf6hezirmy3a.cloudfront.net"
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
