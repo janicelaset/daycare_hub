35b2de0b80b897c867ce86d0acc924c34b614303
Janice Laset-Parkerson <janicelaset@Janice-Laset-Parkersons-MacBook.local>
2015-12-02 12:00:29 -0800

    format teacher page

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < ActiveRecord::Base
   belongs_to :daycare
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "150x150>" }, default_url: ":style/teacher.png"
+  has_attached_file :picture, styles: { medium: "200x200>", thumb: "150x150>" }, default_url: ":style/teacher.png"
 
   validates :name, :presence => true
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
