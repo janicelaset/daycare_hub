089f998051bbf14e09af9f997fab9993ee5c083f
Janice <janicelaset@gmail.com>
2016-01-29 13:45:47 -0800

    allow user to edit certification info

--- app/controllers/certifications_controller.rb
+++ app/controllers/certifications_controller.rb
@@ -3,12 +3,35 @@ class CertificationsController < ApplicationController
     @certification = Certification.new(cert_params)
     @daycare = Daycare.find(params[:daycare_id])
     @certification.daycare = @daycare
-
     @certification.save
+
+    @certifications = @daycare.certifications
 
     #create new certification to display create certification form so users can add more programs
     @certification = Certification.new
 
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def edit
+    @daycare = Daycare.find(params[:daycare_id])
+    @certification = Certification.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def update
+    @certification = Certification.find(params[:id])
+    @daycare = Daycare.find(params[:daycare_id])
+    @certifications = @daycare.certifications
+
+    @certification.update(cert_params)
     respond_to do |format|
       format.html
       format.js
@@ -22,6 +45,6 @@ class CertificationsController < ApplicationController
 
   private
   def cert_params
-    params.require(:certification).permit(:name, :description, :image, :id, :position)
+    params.require(:certification).permit(:name, :description, :link, :image, :id, :position)
   end
 end
