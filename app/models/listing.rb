d347d58fe7c2d7919c5b1bf679b9f483ea89da41
Janice <janicelaset@gmail.com>
2016-03-22 05:15:36 -0700

    geocode listing address on create

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,10 @@
 class Listing < ActiveRecord::Base
+  # attr_accessible :street, :city, :state, :zip, :latitude, :longitude
+  geocoded_by :full_address
+  # after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+  after_validation :geocode
+
+  def full_address
+    "#{street}, #{city}, #{state} #{zip}"
+  end
 end
