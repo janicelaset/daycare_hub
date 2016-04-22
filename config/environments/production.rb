1595fc4cbe913ab881e0b109a7b93796cb002f6f
Janice <janicelaset@gmail.com>
2016-04-22 05:17:25 -0700

    mailgun config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,11 +95,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'www.beebsy.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => "gmail.com",
-    :user_name => ENV['EMAIL_USER'],
-    :password => ENV['EMAIL_PW'],
-    :authentication => 'plain',
-    :enable_starttls_auto => true }
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'beebsy.herokuapp.com',
+    :authentication => :plain,
+  }
 end
